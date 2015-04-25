1941a7d417d781453583b8eca7e4ceedc1d59d54
Olli Huotari <olli.huotari@iki.fi>
2015-04-25 12:38:05 +0300

    fixed bug in using mochas has_method  instead of respond_to?

--- lib/referer_tracking/sweeper.rb
+++ lib/referer_tracking/sweeper.rb
@@ -2,7 +2,7 @@ class RefererTracking::Sweeper < ActionController::Caching::Sweeper
   # https://github.com/rails/rails-observers/blob/master/lib/rails/observers/active_model/observing.rb
 
   def after_create(record)
-    if controller && controller.method_exists?(:referer_tracking_after_create)
+    if controller && controller.respond_to?(:referer_tracking_after_create)
       controller.referer_tracking_after_create(record)
     end
   end
