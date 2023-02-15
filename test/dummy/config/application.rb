ee3ecc435a3df8b342ee97e4c7485fcc7e3d047e
Olli Huotari <olli.huotari@iki.fi>
2023-02-15 15:01:07 +0200

    Rails 7.0 tests fixes

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -36,10 +36,10 @@ module Dummy
     config.filter_parameters += [:password]
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    # config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    # config.assets.version = '1.0'
   end
 end
 
